63dfa7d33db32f1c806003ebc0af1887d3b4914c
expiracy <jameslgray.home@gmail.com>
2021-10-06 22:55:07 +0100

    cluster file

--- detection/Clusters.py
+++ detection/Clusters.py
@@ -1,54 +1,60 @@
 import numpy as np
 import random as rd
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
-
+#https://github.com/pavankalyan1997/Machine-learning-without-any-libraries/blob/master/2.Clustering/1.K_Means_Clustering/K_means_clustering.ipynb
 class Kmeans:
-    def __init__(self, data, K):
-        self.data = data
+    def __init__(self, X, K):
+        self.X = X
         self.Output = {}
-        self.Centroids = np.array([]).reshape(self.data.shape[1], 0)
+        self.centroids = np.array([]).reshape(self.X.shape[1], 0)
         self.K = K
-        self.height = self.data.shape[0]
+        self.m = self.X.shape[0]
 
-    def kmeanspp(self):
-        i = rd.randint(0, self.data.shape[0])
-        Centroid_temp = np.array([self.data[i]])
+    def find_centroids(self):
+        i = rd.randint(0, X.shape[0])
+
+        centroid_temp = np.array([X[i]])
+
         for k in range(1, self.K):
             D = np.array([])
-            for x in self.data:
-                D = np.append(D, np.min(np.sum((x - Centroid_temp) ** 2)))
+
+            for x in self.X:
+                D = np.append(D, np.min(np.sum((x - centroid_temp) ** 2)))
+
             prob = D / np.sum(D)
-            cummulative_prob = np.cumsum(prob)
+            cumulative_prob = np.cumsum(prob)
+
             r = rd.random()
+
             i = 0
-            for j, p in enumerate(cummulative_prob):
+
+            for j, p in enumerate(cumulative_prob):
                 if r < p:
                     i = j
                     break
-            Centroid_temp = np.append(Centroid_temp, [self.data[i]], axis=0)
-        return Centroid_temp.T
 
-    def euclidian_distance(self):
-        pass
+            centroid_temp = np.append(centroid_temp, [self.X[i]], axis=0)
+
+        return centroid_temp.T
 
     def fit(self, n_iter):
         # randomly Initialize the centroids
-        self.Centroids = self.kmeanspp()
-
-        """for i in range(self.K):
-            rand=rd.randint(0,self.m-1)
-            self.Centroids=np.c_[self.Centroids,self.X[rand]]"""
+        self.centroids = self.find_centroids()
 
         # compute euclidian distances and assign clusters
         for n in range(n_iter):
-            EuclidianDistance = np.array([]).reshape(self.height, 0)
+
+            EuclideanDistance = np.array([]).reshape(self.m, 0)
+
             for k in range(self.K):
-                tempDist = np.sum((self.data - self.Centroids[:, k]) ^ 2, axis=1)
 
-                EuclidianDistance = np.c_[EuclidianDistance, tempDist]
+                differences = np.sum((self.X - self.centroids[:, k]) ** 2, axis=1)
 
-            C = np.argmin(EuclidianDistance, axis=1) + 1
+                EuclideanDistance = np.c_[EuclideanDistance, differences]
+
+            C = np.argmin(EuclideanDistance, axis=1) + 1
 
             # adjust the centroids
             Y = {}
@@ -56,54 +62,40 @@ class Kmeans:
             for k in range(self.K):
                 Y[k + 1] = np.array([]).reshape(2, 0)
 
-            for i in range(self.height):
-                Y[C[i]] = np.c_[Y[C[i]], self.data[i]]
+            for i in range(self.m):
+                Y[C[i]] = np.c_[Y[C[i]], self.X[i]]
 
             for k in range(self.K):
                 Y[k + 1] = Y[k + 1].T
 
             for k in range(self.K):
-                self.Centroids[:, k] = np.mean(Y[k + 1], axis=0)
+                self.centroids[:, k] = np.mean(Y[k + 1], axis=0)
 
             self.Output = Y
 
-
-
     def predict(self):
-        return self.Output, self.Centroids.T
+        return self.Output, self.centroids.T
 
     def WCSS(self):
         wcss = 0
         for k in range(self.K):
-            wcss += np.sum((self.Output[k + 1] - self.Centroids[:, k]) ** 2)
+            wcss += np.sum((self.Output[k + 1] - self.centroids[:, k]) ** 2)
         return wcss
 
 
 x_list = [30, 61, 30, 61, 30, 61, 30, 44, 60, 30, 44, 60, 30, 44, 60, 30, 44, 60, 30, 44, 60, 8, 30, 44, 60, 8, 30, 44, 60, 8, 29, 44, 60, 8, 29, 44, 60, 8, 29, 44, 60, 8, 29, 44, 60, 8, 29, 44, 60, 8, 29, 44, 60, 8, 29, 44, 60, 8, 29, 60, 8, 29, 60, 8, 29, 60]
-x_y_list = [[x, 0] for x in x_list]
-x_y_list = np.array(x_y_list)
+X = [[x, 0] for x in x_list]
+X = np.array(X)
 
 
-plt.scatter(x_y_list[:, 0], x_y_list[:, 1], c='black', label='unclustered data')
-plt.show()
+plt.scatter(X[:, 0], X[:, 1], c='black', label='unclustered data')
+#plt.show()
 
-k_means = Kmeans(x_y_list, 4)
-
+K = 4
+k_means = Kmeans(X, K)
 k_means.fit(10)
-
 output, centroids = k_means.predict()
-
-color = ['red', 'blue', 'green', 'cyan', 'magenta']
-labels = ['cluster1', 'cluster2', 'cluster3', 'cluster4', 'cluster5']
 
 print(centroids)
 
-for k in range(4):
-    plt.scatter(output[k+1][:, 0], output[k+1][:, 1], c=color[k], label=labels[k])
 
-for centroid in centroids:
-    plt.scatter(centroid[0], centroid[1], s=100, c='yellow', label='Centroids')
-
-plt.show()
-
-print("BREAK")
